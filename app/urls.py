028cdac05ceccfee1debfb5133d17c033183dd1b
Shivangbijalwan <shivangbijalwan771@gmail.com>
2025-08-12 14:51:50 +0530

    test

--- app/urls.py
+++ app/urls.py
@@ -4,9 +4,9 @@ from django.http import HttpResponse
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('contact', views.contact, name='contact'),
+    path('contact/', views.contact, name='contact'),
     path('signup', views.handleSignUp, name="handleSignUp"),
     path('login', views.handleLogin, name="handleLogin"),
-    path('logout', views.handleLogout, name="handleLogout"),
+    path('logout', views.handleLogout, name="handle"),
     
 ]
